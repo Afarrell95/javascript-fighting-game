38936da365c47c9d69d985f1ccdb723fb5b4b555
Allison <farrella95@gmail.com>
2023-02-10 09:51:01 -0500

    move fowards and back

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ class Sprite {
 
   update() {
     this.drawSprite();
+    this.position.x += this.velocity.x;
     this.position.y += this.velocity.y;
 
     if (this.position.y + this.height + this.velocity.y >= canvas.height) {
@@ -60,12 +61,57 @@ const enemy = new Sprite({
 
 console.log(player);
 
+const keys = {
+  a: {
+    pressed: false,
+  },
+  d: {
+    pressed: false,
+  },
+};
+
 function animate() {
   canvasContext.fillStyle = "black";
   canvasContext.fillRect(0, 0, canvas.width, canvas.height);
   window.requestAnimationFrame(animate);
   player.update();
   enemy.update();
+
+  player.velocity.x = 0;
+
+  if (keys.a.pressed) {
+    player.velocity.x = -1;
+  } else if (keys.d.pressed) {
+    player.velocity.x = 1;
+  }
 }
 
+let lastKey;
+
 animate();
+
+window.addEventListener("keydown", (event) => {
+  switch (event.key) {
+    case "d":
+      keys.d.pressed = true;
+      //   lastKey = "d";
+      break;
+    case "a":
+      keys.a.pressed = true;
+      //   lastKey = "a";
+      break;
+  }
+  console.log(event.key);
+});
+
+window.addEventListener("keyup", (event) => {
+  switch (event.key) {
+    case "d":
+      keys.d.pressed = false;
+      break;
+    case "a":
+      keys.a.pressed = false;
+      break;
+  }
+  console.log(event.key);
+});
